f51adc3c8c36b25b7934d45cc94a37f82b1efcf7
SalmaGr <146674220+SalmaGr@users.noreply.github.com>
2023-11-11 21:46:30 +0100

    tp

--- RMIConversion_Server/src/rmiService/IConversion.java
+++
@@ -1,8 +0,0 @@
-package rmiService;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IConversion extends Remote {
-	public double convertirMontant(double mt) throws RemoteException;
-}
